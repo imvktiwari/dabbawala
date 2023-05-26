9c8acb3d7f2c8cadfda45335170ceb260a60ed63
imvktiwari <imvktiwari2003@gmail.com>
2023-05-26 10:37:10 +0530

    LoginForm

--- dabbawala_frontend/src/components/Forms/LoginForm.js
+++ dabbawala_frontend/src/components/Forms/LoginForm.js
@@ -1,33 +1,97 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+
 import classes from './LoginForm.module.css';
 
-const MealsSummary = () => {
-  return (
-    <section className={classes.summary}>
-      <h2>"Ghar Ka Swaad, Dabbewale Ki Saugaat"</h2>
-      <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at your workplace.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p><p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at your workplace.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p><p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at your workplace.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
-    </section>
-  );
-};
+const LoginForm = () => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
 
-export default MealsSummary;
+
+  const BACKEND_BASE_URL = "http://localhost:4000";
+  const navigate = useNavigate();
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const res = await axios.post(`${BACKEND_BASE_URL}/login`, {
+      email: enteredEmail,
+      password: enteredPassword,
+    });
+    alert(res.data.message);
+    if (res.status === "200") {
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+
+    if (res.data.message === "login successful") {
+      localStorage.setItem(
+        "dabbawala",
+        JSON.stringify(res.data.user)
+      );
+    } else {
+      alert("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage["dabbawala"]) {
+      navigate("/");
+    }
+  }, []);
+
+
+
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <h2>Sign In</h2>
+      <div className={classes.formInputControl}>
+        <div>
+          {/* <label>Email Id</label> */}
+          <br></br>
+          <input
+            type='email'
+            value={enteredEmail}
+            placeholder='Enter your email address'
+            onChange={emailChangeHandler}
+          />
+        </div>
+        <div className={classes.formInputControl}>
+          {/* <label>Password</label> */}
+          <input
+            type='password'
+            placeholder='Enter your password'
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+          />
+        </div>
+      </div>
+
+      <div>
+        <button type='submit' className={classes.submitButton}>Log In</button>
+      </div>
+      <div>
+        <button type="button" className={classes.signupButton} onClick={() => {
+          navigate("/Register");
+        }}><p>Don't have an account ? Sign up here.</p></button>
+      </div>
+    </form>);
+}
+
+export default LoginForm;
+
+
+
+
+
--- dabbawala_frontend/src/components/Layout/Footer.js
+++ dabbawala_frontend/src/components/Layout/Footer.js
@@ -13,7 +13,9 @@ const Footer = () => {
                 Mumbai's Dabbawalas are an iconic and highly efficient lunchbox delivery service in the city of Mumbai, India.
                 The Dabbawalas are a group of individuals who collect freshly cooked meals from the homes of thousands of office workers and deliver them to their workplaces, ensuring that they receive their meals on time.
                 The Dabbawalas operate with remarkable efficiency, delivering thousands of lunchboxes accurately and on time every day.
-            </p></footer>
+            </p>
+            <p >For more information visit : <a href="https://mumbaidabbawala.in/" style={{ color: "white" }}>Mumbai Dabbawalas</a></p>
+        </footer>
     </Fragment>);
 }
 export default Footer
